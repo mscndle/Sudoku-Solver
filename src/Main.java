044a5fb25f110ade4fcb2fa5f8373e63293ed6c6
Mandeep Condle <mandeepcondle@gmail.com>
2014-11-01 21:13:09 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//
+// Run solver from this class
+//
+// Enter sudoku puzzle in the input.txt file with rows on separate
+// lines and using single space between numbers.
+// 0 - indicates empty value
+//
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+}
